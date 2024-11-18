b83843efcc6554489d80cab29dee859996ff1c42
lodug <153726063+lodug@users.noreply.github.com>
2024-11-18 15:30:58 +0200

    Update file that create sdcard image (#11)
    * update of the file that creates sd card
    
    * modifs added
    
    * some prints and venv setup changes

--- autoscripts/create_sd_card.sh
+++ autoscripts/create_sd_card.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 
-# Save initial directory
-INITIAL_DIR="$PWD"
+# The script will be run as: 1. sudo ./create_sd_card.sh (when there is need to set the virtual environment setup)
+# or 2. sudo ./create_sd_card.sh --only-sdcard (when there is no need of virtual environment setup, but only sd card creation). 
+# This script can also be run from any directory.
+
+# Determine the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Function to find a target directory by searching up the tree
+find_directory_upwards()
+{
+  local TARGET_DIR="$1"
+  local CURRENT_DIR="$SCRIPT_DIR"
+  
+  while [[ "$CURRENT_DIR" != "/" ]]; do
+    if [[ -d "$CURRENT_DIR/$TARGET_DIR" ]]; then
+    echo "$CURRENT_DIR/$TARGET_DIR"
+    return 0
+    fi
+  # Go up one level
+  CURRENT_DIR="$(dirname "$CURRENT_DIR")"
+  done
+  
+  #Return 1 if not found
+  return 1
+}
 
 # Ensure the script is run with sudo
 if [ "$EUID" -ne 0 ]; then
   echo "Please run with sudo"
   exit
+fi
+
+# Check the only sdcard argument - run the script with "--only-sdcard" argument if there is no need of virtual environment setup
+ONLY_SDCARD=false
+if [ "$1" == "--only-sdcard" ]; then
+  ONLY_SDCARD=true
 fi
 
 PYTHON_VERSION=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
@@ -19,49 +48,66 @@ fi
 
 echo "Python version is $PYTHON_VERSION. Proceeding..."
 
-# Step 1: Change directory to ../rest_api
-cd ../rest_api || { echo "rest_api directory not found. Exiting."; exit 1; }
+# Skip venv setup if --only-sdcard is provided
+if [ "$ONLY_SDCARD" = false ]; then
+  # Step 1: Change directory to ../rest_api, but first find the rest_api directory by searching upwards from the script location
+  REST_API_DIR=$(find_directory_upwards "rest_api")
+ 
+  if [[ -z "$REST_API_DIR" ]]; then
+    echo "rest_api directory not found. Exiting."
+    exit 1
+  else
+    echo "Found rest_api directory at $REST_API_DIR"
+  fi
 
-# Step 2
-# Check if the virtual environment exists
-if [ -d "venv" ]; then
-    echo "Creating virtual environment..."
-    python3.8 -m venv venv
-    # Activate the virtual environment
-    source venv/bin/activate
-    echo "Virtual environment activated."
+  cd "$REST_API_DIR" || exit 1
 
-    # Optional: Check if the activation was successful
-    if [ "$VIRTUAL_ENV" != "" ]; then
-        echo "Currently in virtual environment: $VIRTUAL_ENV"
-        pip install -r requirements.txt
-    else
-        echo "Failed to activate the virtual environment."
-    fi
-else
-    echo "Virtual environment not found. Please create it using 'python3 -m venv venv'."
-fi
+  # Step 2
+  # Check if the virtual environment exists
+  if [ ! -d "venv" ]; then
+      echo "Creating virtual environment..."
+      python3.8 -m venv venv
+      # Activate the virtual environment
+      source venv/bin/activate
+      echo "Virtual environment activated."
+      pip install -r requirements.txt
+
+      # Optional: Check if the activation was successful
+      if [ "$VIRTUAL_ENV" != "" ]; then
+          echo "Virtual environment activated: $VIRTUAL_ENV"
+          pip install -r requirements.txt
+          echo "Requirements installed."
+      else
+          echo "Failed to activate the virtual environment."
+      fi
+  else
+      echo "Virtual environment found."
+      # Activate the virtual environment
+      source venv/bin/activate
+      if [ "$VIRTUAL_ENV" != "" ]; then
+          echo "Virtual environment activated: $VIRTUAL_ENV"
+      else
+          echo "Failed to activate the virtual environment."
+      fi
+  fi
 
 # Step 4: Change directory back to ../mcu
-cd ../mcu || { echo "mcu directory not found. Exiting."; exit 1; }
+MCU_DIR=$(find_directory_upwards "mcu")
+ 
+  if [[ -z "$MCU_DIR" ]]; then
+    echo "mcu directory not found. Exiting."
+    exit 1
+  else
+    echo "Found mcu directory at $MCU_DIR"
+  fi
 
-# Ensure we are back in the initial directory
-cd "$INITIAL_DIR"
+  cd "$MCU_DIR" || exit 1
 
-# Step 5: Navigate backwards to find the Desktop directory
-while [[ "$PWD" != "/" && "${PWD##*/}" != "Desktop" ]]; do
-  cd ..
-done
-
-# Check if we are in the Desktop directory
-if [[ "${PWD##*/}" != "Desktop" ]]; then
-  echo "Desktop directory not found. Exiting."
-  exit 1
 fi
 
-echo "Found Desktop directory at $PWD"
-
-IMG_PATH="sdcard.img"
+#Set IMG_PATH in order to create the sd card image in the user's home directory
+USER_HOME=$(eval echo ~"$SUDO_USER")
+IMG_PATH="$USER_HOME/sdcard.img"
 LOOP_DEVICE=""
 PART1_SIZE="+100M"
 START_SECTOR=2048
@@ -69,109 +115,95 @@ SEEK_OFFSET=118006272
 DD_COUNT=17168
 MOUNT_DIR="/mnt/sdcard"
 
-# Step 6: Search and remove any existing loop device associated with sdcard.img
-EXISTING_LOOP_DEVICE=$(losetup -j "$IMG_PATH" | cut -d: -f1)
+# Step 7: Check if the image already exists
+if [ -f "$IMG_PATH" ]; then
+   echo "Image already exists at $IMG_PATH."
+   
+  # Step 6: Search and remove any existing loop device associated with sdcard.img
+  EXISTING_LOOP_DEVICE=$(losetup -j "$IMG_PATH" | cut -d: -f1)
 
-if [ -n "$EXISTING_LOOP_DEVICE" ]; then
-  echo "Found existing loop device: $EXISTING_LOOP_DEVICE."
+  if [ -n "$EXISTING_LOOP_DEVICE" ]; then
+    echo "Found existing loop device: $EXISTING_LOOP_DEVICE."
+    
+    # Detach the loop device
+    echo "Detaching loop device $EXISTING_LOOP_DEVICE..."
+    sudo losetup -d "$EXISTING_LOOP_DEVICE"
 
-  # Unmount any partitions associated with the loop device
-  MOUNTED_PARTITIONS=$(lsblk -lno NAME,MOUNTPOINT | grep "^$(basename $EXISTING_LOOP_DEVICE)" | awk '{print $2}')
-  if [ -n "$MOUNTED_PARTITIONS" ]; then
-    echo "Unmounting partitions:"
-    for MOUNT_POINT in $MOUNTED_PARTITIONS; do
-      echo "Unmounting $MOUNT_POINT..."
-      sudo umount "$MOUNT_POINT"
-    done
+    # Verify that the loop device has been successfully detached
+    if losetup -a | grep -q "$IMG_PATH"; then
+      echo "Loop device $EXISTING_LOOP_DEVICE could not be detached. Forcing detachment..."
+      sudo losetup -D  # Force-detach all loop devices
+    fi
   fi
 
-  # Reformatting the partitions
-  LOOP_PARTITIONS=$(lsblk -lno NAME | grep "^$(basename $EXISTING_LOOP_DEVICE)p")
-  if [ -n "$LOOP_PARTITIONS" ]; then
-    echo "Reformatting partitions:"
-    for PARTITION in $LOOP_PARTITIONS; do
-      echo "Reformatting /dev/$PARTITION..."
-      sudo mkfs.fat -F 32 "/dev/$PARTITION"  # Reformat as FAT32 (adjust if needed)
-    done
-  else
-    echo "No partitions found for $EXISTING_LOOP_DEVICE."
-  fi
+  LOOP_DEVICE=$(losetup -fP --show "$IMG_PATH")
+   # Extract the loop number from LOOP_DEVICE
+  LOOP_NUMBER=$(basename "$LOOP_DEVICE" | grep -o '[0-9]*')
+  #Change permissions of the loop device
+  sudo chmod 777 "$LOOP_DEVICE"
+  
+else
 
-  # Detach the loop device
-  echo "Detaching loop device $EXISTING_LOOP_DEVICE..."
-  sudo losetup -d "$EXISTING_LOOP_DEVICE"
+  echo "Creating image..."
+  sudo truncate -s +300M "$IMG_PATH"
 
-  # Verify that the loop device has been successfully detached
-  if losetup -a | grep -q "$IMG_PATH"; then
-    echo "Loop device $EXISTING_LOOP_DEVICE could not be detached. Forcing detachment..."
-    sudo losetup -D  # Force-detach all loop devices
-  fi
+  # Step 8: Create a loop device
+  LOOP_DEVICE=$(losetup -fP --show "$IMG_PATH")
+  echo "Loop device created: $LOOP_DEVICE"
+
+  # Extract the loop number from LOOP_DEVICE
+  LOOP_NUMBER=$(basename "$LOOP_DEVICE" | grep -o '[0-9]*')
+
+  # Step 10: Create partitions
+  echo "Creating partitions..."
+  (
+  echo n # New partition
+  echo p # Primary
+  echo 1 # Partition number
+  echo $START_SECTOR # First sector
+  echo $PART1_SIZE # Last sector
+  echo n # New partition
+  echo p # Primary
+  echo 2 # Partition number
+  echo   # First sector (Accept default)
+  echo   # Last sector (Accept default)
+  echo w # Write changes
+  ) | sudo fdisk "$LOOP_DEVICE"
+
+  sudo partprobe "$LOOP_DEVICE"
+
+  # Step 11: Formatting the partitions
+  echo "Formatting partitions..."
+  sudo mkfs.fat -F 32 "${LOOP_DEVICE}p1"
+  sudo mkfs.fat -F 16 "${LOOP_DEVICE}p2"
+
+  # Step 12: Change permissions of the loop device
+  sudo chmod 777 "$LOOP_DEVICE"
+
+  # Step 13: Create mount directory
+  echo "Creating mount directory..."
+  mkdir -p "$MOUNT_DIR"
+
+  # Step 14: Mount partitions
+  echo "Mounting partitions..."
+  sudo mount -o rw,uid=1000,gid=1000 "${LOOP_DEVICE}p1" "$MOUNT_DIR"
+  sudo mount -o rw,uid=1000,gid=1000 "${LOOP_DEVICE}p2" "$MOUNT_DIR"
+
+  # Step 15: Display partition data
+  # echo "Reading data from offset $SEEK_OFFSET..."
+  # xxd -l $DD_COUNT -s $SEEK_OFFSET "$LOOP_DEVICE"
+
+  # Step 16: Zero out the data at specified offset
+  # echo "Zeroing out data at offset $SEEK_OFFSET..."
+  # sudo dd if=/dev/zero of="$LOOP_DEVICE" bs=1 seek=$SEEK_OFFSET count=$DD_COUNT conv=notrunc
+
+  # Display the created partitions
+  echo "Partitions created:"
+  sudo lsblk -lno NAME,TYPE,SIZE "$LOOP_DEVICE"
 fi
 
-# Step 7: Check if the image already exists
-# if [ -f "$IMG_PATH" ]; then
-  # echo "Image already exists at $IMG_PATH."
-sudo rm "$IMG_PATH"
-
-echo "Creating image..."
-sudo truncate -s +300M "$IMG_PATH"
-
-# Step 8: Create a loop device
-LOOP_DEVICE=$(losetup -fP --show "$IMG_PATH")
-echo "Loop device created: $LOOP_DEVICE"
-
-# Extract the loop number from LOOP_DEVICE
-LOOP_NUMBER=$(basename "$LOOP_DEVICE" | grep -o '[0-9]*')
-
-# Step 10: Create partitions
-echo "Creating partitions..."
-(
-echo n # New partition
-echo p # Primary
-echo 1 # Partition number
-echo $START_SECTOR # First sector
-echo $PART1_SIZE # Last sector
-echo n # New partition
-echo p # Primary
-echo 2 # Partition number
-echo   # First sector (Accept default)
-echo   # Last sector (Accept default)
-echo w # Write changes
-) | sudo fdisk "$LOOP_DEVICE"
-
-sudo partprobe "$LOOP_DEVICE"
-
-# Step 11: Formatting the partitions
-echo "Formatting partitions..."
-sudo mkfs.fat -F 32 "${LOOP_DEVICE}p1"
-sudo mkfs.fat -F 16 "${LOOP_DEVICE}p2"
-
-# Step 12: Change permissions of the loop device
-sudo chmod 777 "$LOOP_DEVICE"
-
-# Step 13: Create mount directory
-echo "Creating mount directory..."
-mkdir -p "$MOUNT_DIR"
-
-# Step 14: Mount partitions
-echo "Mounting partitions..."
-sudo mount -o rw,uid=1000,gid=1000 "${LOOP_DEVICE}p1" "$MOUNT_DIR"
-sudo mount -o rw,uid=1000,gid=1000 "${LOOP_DEVICE}p2" "$MOUNT_DIR"
-
-# Step 15: Display partition data
-# echo "Reading data from offset $SEEK_OFFSET..."
-# xxd -l $DD_COUNT -s $SEEK_OFFSET "$LOOP_DEVICE"
-
-# Step 16: Zero out the data at specified offset
-# echo "Zeroing out data at offset $SEEK_OFFSET..."
-# sudo dd if=/dev/zero of="$LOOP_DEVICE" bs=1 seek=$SEEK_OFFSET count=$DD_COUNT conv=notrunc
-
-# Display the created partitions
-echo "Partitions created:"
-sudo lsblk -lno NAME,TYPE,SIZE "$LOOP_DEVICE"
-
 # Step 17: Navigate back to initial location
-cd "$INITIAL_DIR"
+cd "$SCRIPT_DIR"
 
 # Step 18: Navigate backwards to find the 'src' directory
 while [[ "$PWD" != "/" && "${PWD##*/}" != "src" ]]; do
@@ -187,20 +219,48 @@ fi
 echo "Found 'src' directory at $PWD"
 SRC_DIR="$PWD"
 
-# Step 19: Replace loop numbers in files, excluding MCULogs.log
-echo "Replacing loop numbers in files, excluding MCULogs.log..."
-find . -type f ! -name "MCULogs.log" -exec sed -i "s/\/dev\/loop[0-9]*/\/dev\/loop${LOOP_NUMBER}/g" {} +
+# Step 19: Replace loop numbers in file MemoryManager.h
+echo "Replacing loop numbers in file MemoryManager.h"
+find ./backend/utils/include -type f -name "MemoryManager.h" -exec sed -i "s/\/dev\/loop[0-9]*/\/dev\/loop${LOOP_NUMBER}/g" {} +
 
 # Navigate to SRC_DIR, go downwards into mcu folder, and run make commands
 cd "$SRC_DIR" || { echo "'src' directory not found. Exiting."; exit 1; }
 
 # Go downwards into mcu folder
 cd ./backend/mcu || { echo "'mcu' directory not found. Exiting."; exit 1; }
+# Run make clean command for mcu
+echo "Running make command for mcu..."
+make clean
 
-# Run make file and make commands
-echo "Running make commands..."
-# make clean
-make
-echo "Build completed successfully."
+# Go downwards into battery folder
+cd ./../ecu_simulation/BatteryModule || { echo "'BaterryModule' directory not found. Exiting."; exit 1; }
+# Run make clean command for battery
+echo "Running make command for battery..."
+make clean
+
+# Go downwards into doors folder
+cd ./../DoorsModule || { echo "'DoorsModule' directory not found. Exiting."; exit 1; }
+# Run make clean command for doors
+echo "Running make command for doors..."
+make clean
+
+# Go downwards into engine folder
+cd ./../EngineModule || { echo "'EngineModule' directory not found. Exiting."; exit 1; }
+# Run make clean command for engine
+echo "Running make command for engine..."
+make clean
+
+# Go downwards into HVAC folder
+cd ./../HVACModule || { echo "'HVACodule' directory not found. Exiting."; exit 1; }
+# Run make clean command for HVAC
+echo "Running make command for HVAC..."
+make clean
+
+echo "Make clean done for mcu and ECUs"
+
+# Info message:
+YELLOW='\033[0;33m'
+NC='\033[0m'
+echo -e "${YELLOW}MCU and ECUs need to be compiled. Run the command 'make'.${NC}"
 
 echo "Script completed successfully."
