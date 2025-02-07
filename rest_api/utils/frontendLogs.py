8f6850cf3c03adf018324784eecde65aa46d8225
nicolae.cernat <nicolae.cernat@randstaddigital.com>
2025-02-07 11:36:29 +0200

    fixing lint issues

--- rest_api/utils/frontendLogs.py
+++ rest_api/utils/frontendLogs.py
@@ -4,8 +4,8 @@ import shutil
 from datetime import datetime
 
 
-
 log_size_uncompressed = 0
+
 
 def uploadFrontendLogs():
     PROJECT_SRC_PATHY = os.path.abspath(os.path.join(os.getcwd(), "..", ".."))
@@ -13,9 +13,9 @@ def uploadFrontendLogs():
     sys.path.append(google_drive_api_path)
     from GoogleDriveApi import gDrive, FRONTEND_LOG_FILE_LOCATION
     current_date = datetime.now().strftime("%d-%m-%Y_%H:%M")
-    
 
-    directory_path = os.path.expanduser(PROJECT_SRC_PATHY + '/backend/rest_api/utils/log' )
+    directory_path = os.path.expanduser(
+        PROJECT_SRC_PATHY + '/backend/rest_api/utils/log')
     for file in os.listdir(directory_path):
 
         if "logs" in file:
@@ -23,4 +23,8 @@ def uploadFrontendLogs():
             new_file_path = os.path.join(directory_path, new_filename)
             old_file_path = os.path.join(directory_path, file)
             shutil.move(old_file_path, new_file_path)
-            gDrive.uploadFile(new_filename, new_file_path, log_size_uncompressed, FRONTEND_LOG_FILE_LOCATION)
+            gDrive.uploadFile(
+                new_filename,
+                new_file_path,
+                log_size_uncompressed,
+                FRONTEND_LOG_FILE_LOCATION)
